c0a32d4c852251fabf659738a7c48390679032a3
Gaétan <gaetan.gerard27@gmail.com>
2019-05-14 20:39:52 +0200

    Find a solution by my own to my slider problem

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,4 +34,94 @@ $(document).ready(function() {
             $(this).next().removeClass("active");
         }
     });
+
+    // ! fonction pour naviguer dans le slider
+
+    let slider = $(".recentPost-container");
+    let slider_content = slider.children(".recentPost");
+    let prev = $(".sliderNav-item.left");
+    let next = $(".sliderNav-item.right");
+    let currentPosition = 0;
+    let count = 0;
+
+
+    next.on("click", function() {
+
+        if (currentPosition === count) {
+            count++;
+            currentPosition++;
+
+            console.log(slider_content[currentPosition - 1]);
+            console.log(currentPosition);
+
+            nextPosition(currentPosition);
+
+            if(count >= 3) {
+                resetPositive();
+            }
+        }
+    });
+
+    prev.on("click", function () {
+
+        if (currentPosition === count) {
+            count--
+            currentPosition--
+
+            console.log(currentPosition);
+            console.log(slider_content[currentPosition - 1]);
+
+            if(currentPosition === 1) {
+                slider_content.animate({"left" : "0"});
+            } else {
+                prevPosition(currentPosition);
+
+                if (count <= 3) {
+                    resetNegative();
+                }
+            }
+        }
+    });
+
+    function resetPositive () {
+        if (count >= 3) {
+            count = 0;
+            currentPosition = 0;
+        }
+    }
+
+    function resetNegative() {
+        if (count <= -3) {
+            count = 0;
+            currentPosition = 0;
+        }
+    }
+
+    function nextPosition(currentPosition) {
+        switch (currentPosition) {
+            case 1:
+                slider_content.animate({"left" : "-640px"});
+                break;
+            case 2:
+                slider_content.animate({ "left": "-1280px" });
+                break;
+            default:
+                slider_content.animate({ "left": "0" });
+                break;
+        }
+    }
+
+    function prevPosition(currentPosition) {
+        switch (currentPosition) {
+            case -1:
+                slider_content.animate({ "left": "-1280px" });
+                break;
+            case -2:
+                slider_content.animate({ "left": "-640px" });
+                break;
+            default:
+                slider_content.animate({ "left": "0" });
+                break;
+        }
+    }  
 });
